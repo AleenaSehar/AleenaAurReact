a14c4794809746db0f438e418d56946d73dbb345
AleenaSehar <aleenasehar319@gmail.com>
2024-08-07 22:39:32 +0500

    here are the changes i made

---
+++ 01basicreact/src/App.js
@@ -0,0 +1,13 @@
+
+import Chai from "./chai"
+function App() {
+  return (
+    <>
+    <Chai/>
+    <h1>Hello aleena meow meow</h1>
+    </>
+    
+  );
+}
+
+export default App;
---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,15 @@
+import Chai from "./chai"
+
+function App() {
+
+  return (
+    <>
+    <Chai/>
+    <h1>chai aur react</h1>
+    <h2> new paragraph</h2>
+    </>
+ 
+  )
+}
+
+export default App
