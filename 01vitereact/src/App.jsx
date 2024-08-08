52c65c1a0428b7f86f6a7df3648d784f02a0cf01
AleenaSehar <aleenasehar319@gmail.com>
2024-08-08 14:04:19 +0500

    new changings

--- 01vitereact/src/App.jsx
+++ 01vitereact/src/App.jsx
@@ -1,11 +1,11 @@
 import Chai from "./chai"
 
 function App() {
-
+  const username="chai aur code"
   return (
     <>
     <Chai/>
-    <h1>chai aur react</h1>
+    <h1>chai aur react {username}</h1>
     <h2> new paragraph</h2>
     </>
  
--- 01vitereact/src/main.jsx
+++ 01vitereact/src/main.jsx
@@ -2,9 +2,39 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
+function MyApp(){
+  return(
+    <div>
+      <h1>custom app</h1>
+    </div>
+  )
+}
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+
+// const reactElement={
+//   type:'a',
+//   props:{
+//       href:'https://google.com',
+//       target:'_blank'
+//   },
+//   children:'Click me to visit google'
+// }
+
+const anotherElement=(
+  <a href='https://google.com' target='_blank'>Visit google</a>
+)
+
+const anotheruser="chai aur react"
+const reactElement=React.createElement(
+  'a',
+  {href:'https://google.com', target: '_blank'},
+  'click me to visit google',
+  anotheruser
+)
+
+ReactDOM.createRoot(document.getElementById('root'))
+.render(
+
+  reactElement
+
 )
